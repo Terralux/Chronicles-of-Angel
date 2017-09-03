bdfe24a808c6372a8682b245f0b446d8bf809950
Terralux <kenneth@berle.eu>
2017-09-03 16:36:06 +0200

    Added Chronicles of Angel initial setup

---
+++ Chronicles of Angel/Assets/Scripts/MenuHandler.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuHandler : MonoBehaviour {
+
+    public GameObject settingsPanel;
+
+    public void StartGame() {
+        Toolbox.FindRequiredComponent<GameMaster>().InitiateStory();
+    }
+
+	public void LoadGame(){
+		Debug.LogWarning ("The loading feature is not implemented yet!");
+	}
+
+    public void ShowSettings() {
+		settingsPanel.SetActive(true);
+    }
+
+    public void Exit() {
+        Application.Quit();
+    }
+}
